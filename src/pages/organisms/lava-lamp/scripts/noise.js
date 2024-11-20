bc4b003cf63054e81b4b9d8c055a480ae59c47c4
badekan <bastien.dekan@gmail.com>
2024-11-20 16:45:04 +0100

    feat: add lava lamp component

---
+++ src/pages/organisms/lava-lamp/scripts/noise.js
@@ -0,0 +1,40 @@
+// technique for this demo found here 
+// http://stackoverflow.com/questions/22003491/animating-canvas-to-look-like-tv-noise    
+const app = document.querySelector('#lavaLamp');
+const canvas = document.createElement('canvas');
+canvas.setAttribute('id','noise');
+
+let ctx = canvas.getContext('2d');
+
+canvas.width = canvas.height = 128;
+
+app.appendChild(canvas);
+
+resize();
+window.onresize = resize;
+
+function resize() {
+	canvas.width = window.innerWidth * window.devicePixelRatio
+	canvas.height = window.innerHeight * window.devicePixelRatio
+	canvas.style.width = app.innerWidth + 'px'
+	canvas.style.height = app.innerHeight + 'px'
+}
+
+function noise(ctx) {
+    
+	const w = ctx.canvas.width,
+				h = ctx.canvas.height,
+				iData = ctx.createImageData(w, h),
+				buffer32 = new Uint32Array(iData.data.buffer),
+				len = buffer32.length
+	  let i = 0
+
+	for(; i < len;i++)
+		if (Math.random() < 0.7) buffer32[i] = 0xffffffff;
+		ctx.putImageData(iData, 0, 0);
+}
+
+(function loop() {
+    noise(ctx);
+    requestAnimationFrame(loop);
+})();
